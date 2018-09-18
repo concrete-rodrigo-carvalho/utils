f7a394edf9ab13993fb4e1cf449c54591a02a05c
Rodrigo Carvalho <33631620+concrete-rodrigo-carvalho@users.noreply.github.com>
2018-09-18 19:07:00 -0300

    Create image_prune.sh

---
+++ docker/image_prune.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# echo "Removing dangling images"
+docker system prune -f || exit 1
+
+# echo "Getting name of repositories"
+repositories=$(docker images --format "{{.Repository}}" | sort | uniq)
+
+# echo "Checking if exist some repositories"
+if [ -z "${repositories}" ]; then
+	echo "0 repositories found"
+	exit 0
+else
+	for repository in ${repositories}; do
+		# echo "Getting tags of each repository and ordering for created datetime"
+		tags=$(docker images ${repository} --format "{{.CreatedAt}} {{.Tag}}" | sort -r | awk '{print $NF}')
+		i=0
+		for tag in ${tags}; do
+			# echo "Ignoring the first image, the most recent"
+			if [[ ${i}>0 ]]; then
+				# echo "Removing image ${repository}:${tag}"
+				docker rmi ${repository}:${tag} || exit 1
+			else
+				echo "### Keeping image ${repository}:${tag}"
+			fi
+			i=${i}++
+		done
+	done
+	echo
+	docker images
+fi
